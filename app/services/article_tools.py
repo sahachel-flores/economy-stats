20b6f658414daf7cadacc140a04d2b7ee44d8d57
sahachel-flores <sahachel.j.flores@gmail.com>
2025-09-26 14:08:34 -0700

    fixed the DB fetching problem

--- app/services/article_tools.py
+++ app/services/article_tools.py
@@ -3,11 +3,15 @@
 from newspaper import Article
 from app.services.logger import api_logger as logger
 from app.db.session import get_db
+from app.db.session import SessionLocal
 from app.models.db_schema import NewsArticles
 from sqlalchemy import text
 
 
 def get_article_text(url: str) -> str:
+    """
+    This function gets the text of the article from the url.
+    """
     try:
         article = Article(url)
         article.download()
@@ -18,28 +22,33 @@ def get_article_text(url: str) -> str:
         return None
 
 def get_article_summary(url: str) -> str:
+    """
+    This function gets the summary of the article from the url.
+    """
     article = Article(url)
     article.download()
     article.parse()
     return article.summary
 
 def get_articles_from_ids(ids: list[str]) -> list[dict]:
+    """
+    This function gets the article from the database using the ids.
+    """
    
     try:
-        db = get_db()
-        articles = []
-        logger.info(f"The ids are: {ids}")
-        for id in ids:
-            try:
-                logger.info(f"The id is: {id}")
-                #article = db.query(NewsArticles).filter(NewsArticles.id == id).all()
-                article = db.execute(text(f"SELECT * FROM news_articles where id == '{id}'")).fetchall()
-                article = article[0]._asdict()
-                logger.info(f"The article is: \n{article}")
-            except Exception as e:
-                logger.error(f"Database Error getting article {id}: {e}")
-                continue
-            articles.append(article)
+        with SessionLocal() as db:
+            articles = []
+            logger.info(f"The ids are: {ids}")
+            for id in ids:
+                try:
+                    logger.info(f"The id is: {id}")
+                    #article = db.query(NewsArticles).filter(NewsArticles.id == id).first()
+                    article = db.execute(text(f"SELECT * FROM news_articles where id == '{id}'")).fetchall()
+                    article = article[0]._asdict()
+                except Exception as e:
+                    logger.error(f"Error fetching one of the id:{id}: {e}")
+                    continue
+                articles.append(article)
         return articles
     except Exception as e:
         logger.error(f"DB Error getting articles from ids: {e}")
--- test.py
+++ test.py
@@ -1,7 +1,12 @@
 import requests
+from app.database import get_db
 
-url = "https://api.stlouisfed.org/fred/category?category_id=125&api_key=73710b7590369eb9a43c051edcb204c4&file_type=json"
+db = get_db()
 
-response = requests.get(url)
+article = db.execute(text(f"SELECT * FROM news_articles where id == '{id}'")).fetchall()
 
-print(response.json())
+# url = "https://api.stlouisfed.org/fred/category?category_id=125&api_key=73710b7590369eb9a43c051edcb204c4&file_type=json"
+
+# response = requests.get(url)
+
+# print(response.json())
